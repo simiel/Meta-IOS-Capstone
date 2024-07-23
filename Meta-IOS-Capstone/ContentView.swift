2d444eb9f748b0de664955c5dc5384a970d0c03a
Simiel <samensah036@st.ug.edu.gh>
2024-07-23 16:42:43 +0000

    feat: onboarding added & persisted

--- Meta-IOS-Capstone/ContentView.swift
+++ Meta-IOS-Capstone/ContentView.swift
@@ -10,76 +10,43 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+    @State private var isOnboardingComplete: Bool = false
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        Group {
+                    if isOnboardingComplete {
+                        MainView()
+                    } else {
+                        OnboardingView(isOnboardingComplete: $isOnboardingComplete)
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                .onAppear {
+                    checkOnboardingStatus()
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+    
+    func checkOnboardingStatus() {
+        let firstName = UserDefaults.standard.string(forKey: "firstName")
+        let lastName = UserDefaults.standard.string(forKey: "lastName")
+        let email = UserDefaults.standard.string(forKey: "email")
+        
+        isOnboardingComplete = firstName != nil && lastName != nil && email != nil
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+    
+    
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+struct MainView: View {
+    var body: some View {
+        Text("Main Content")
+            .font(.largeTitle)
+    }
+}
 
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
--- Meta-IOS-Capstone/Views/OnboardingView.swift
+++ Meta-IOS-Capstone/Views/OnboardingView.swift
@@ -5,14 +5,66 @@
 //  Created by Samuel Mensa on 23/07/2024.
 //
 
+//let kFirstName = "first name key"
+//let kLastName = "last name key"
+//let kEmail = "email key"
+
 import SwiftUI
 
 struct OnboardingView: View {
+    @State private var firstName: String = ""
+    @State private var lastName: String = ""
+    @State private var email: String = ""
+    @Binding var isOnboardingComplete: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Welcome to Little Lemon!")
+                .font(.largeTitle)
+                .padding()
+            
+            TextField("Enter your first name", text: $firstName )
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            TextField("Enter your last name", text: $lastName )
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            TextField("Enter your email address", text: $email )
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .keyboardType(.emailAddress)
+                .autocapitalization(.none)
+            
+            Button(action: {
+                if !firstName.isEmpty && !lastName.isEmpty && !email.isEmpty {
+                                    saveUserData()
+                                    isOnboardingComplete = true
+                                } else {
+                                    print("All fields must be filled out.")
+                                }
+            }){
+                Text("Login")
+                    .font(.headline)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .padding()
+        }
+        .padding()
+        
+    }
+    
+    func saveUserData(){
+        UserDefaults.standard.set(firstName, forKey: "firstName")
+        UserDefaults.standard.set(lastName, forKey: "lastName")
+        UserDefaults.standard.set(email, forKey: "email")
     }
 }
 
 #Preview {
-    OnboardingView()
+    OnboardingView(isOnboardingComplete: .constant(false))
 }
